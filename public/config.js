ffcef2881d484cef42a9a3b6f58ccd0c4ae773a3
wjjwkwindy <wjjwkwindy@gmail.com>
2020-05-29 10:53:25 +0800

    添加 ts 网站监测、修改页脚信息

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: 'Co2oc Status',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,10 +10,9 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
     'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm785057549-d41584fccd07431419c3f9da',
+    'm785058516-a8e6f3aa60fed54412afebe7',
   ],
 
   // 是否显示监测站点的链接
--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className="container">
-        <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 5 分钟</p>
-        <p>&copy; 2020 <Link to="https://status.org.cn/" text="STATUS.ORG.CN" /></p>
+        <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 5 分钟。PoweredBy <Link to="https://github.com/yb/uptime-status" text="Uptime Status" /></p>
+        <p>&copy; 2020 <Link to="http://status.co2oc.com/" text="STATUS.CO2OC.COM" /></p>
       </div>
     </div>
   );
